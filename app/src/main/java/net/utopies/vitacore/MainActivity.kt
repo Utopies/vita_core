51f44976802d030fcf5f81b4da6be9280a14d39a
Utopies <etotgus0@gmail.com>
2025-04-19 14:28:48 +0300

    add logiс for btn_stop

--- app/src/main/java/net/utopies/vitacore/MainActivity.kt
+++ app/src/main/java/net/utopies/vitacore/MainActivity.kt
@@ -4,22 +4,25 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.IBinder
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import net.utopies.vitacore.R.layout.main_layout
 import net.utopies.vitacore.StepCounter.StepCounterService
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
     private var stepCounterService: StepCounterService? = null
-    private var isBound = false
+    private var isBoundStepServer = false
+    private var isWorkedStepServer = false
     private lateinit var tvStepCount: TextView
     private var lastUpdateTime: Long = 0
     private val updateInterval = 1000L // 1 секунда
+    private lateinit var btnStop: Button
 
     private val updateSteps = { steps: Int ->
         val currentTime = System.currentTimeMillis()
@@ -34,12 +37,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        // Запустить и привязать сервис
-        Intent(this, StepCounterService::class.java).also { intent ->
-            startForegroundService(intent) // Для долгой работы в фоне
-            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
-        }
     }
 
     @SuppressLint("SetTextI18n")
@@ -47,18 +44,57 @@ class MainActivity : ComponentActivity() {
         super.onStart()
         setContentView(main_layout)
         tvStepCount = findViewById(R.id.tv_step_count)
-        updateSteps.invoke(stepCounterService?.countStep ?: 0)
+        buttonRegister()
 
+        updateSteps.invoke(stepCounterService?.countStep ?: 0)
         tvStepCount.text = "Шаги: ${stepCounterService?.countStep?: 0}"
     }
 
     override fun onDestroy() {
         super.onDestroy()
         stepCounterService?.unregisterListener(updateSteps)
-        if (isBound) {
+        if (isBoundStepServer) {
             unbindService(serviceConnection)
-            isBound = false
+            isBoundStepServer = false
         }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_stop -> {
+                if (isWorkedStepServer == true)
+                    onStopStepService()
+                else
+                    onStartStepService()
+            }
+        }
+    }
+
+    private fun buttonRegister(){
+        btnStop = findViewById(R.id.btn_stop)
+        btnStop.setOnClickListener(this)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun onStopStepService(){
+        if(isBoundStepServer == true)
+            unbindService(serviceConnection)
+        Intent(this, StepCounterService::class.java).also { intent ->
+            stopService(intent)
+        }
+        btnStop.text = "Start"
+        isWorkedStepServer = false
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun onStartStepService(){
+        Intent(this, StepCounterService::class.java).also { intent ->
+            startForegroundService(intent) // Для долгой работы в фоне
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
+
+        btnStop.text = "Stop"
+        isWorkedStepServer = true
     }
 
     private val serviceConnection = object : ServiceConnection {
@@ -66,13 +102,14 @@ class MainActivity : ComponentActivity() {
             val binder = service as StepCounterService.LocalBinder
             stepCounterService = binder.getService()
             stepCounterService?.registerListener(updateSteps)
-            isBound = true
+            isBoundStepServer = true
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
             stepCounterService?.unregisterListener(updateSteps)
             stepCounterService = null
-            isBound = false
+            isBoundStepServer = false
         }
     }
+
 }
